0218deeb86e0adb12ab74e9a90151f9f92771285
keschaefer <keschaefer@gmail.com>
2018-12-06 11:50:52 -0700

    initial commit, added header and main homepage image

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+      <div className= 'header'>
+         <header>Kate's Movie Picks</header>
+      </div>
+   )
+}
+
+export default Header
---
+++ src/Components/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => {
+   return(
+      <img src='https://media.vanityfair.com/photos/56afc980f5d299ff0d00cc8f/master/w_768,c_limit/FINAL-hollywood-portfolio-2016-vf-cover-annie-leibovitz-jennifer-lawrence-viola-davis-jane-fonda.jpg' alt='women in film 2016 Vanity Fair Cover'></img>
+   )
+}
+
+export default Main
